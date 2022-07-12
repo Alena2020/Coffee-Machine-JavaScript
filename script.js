1290166b6b5d0dc6b84a52e21d55231820428500
Alena <elenila_skinef@mail.ru>
2022-07-12 21:27:42 +0300

    calculated how much water, coffee, and milk are necessary to make the specified amount of coffee.

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const input = require('sync-input');
 
-console.log(`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`);
+// console.log(`Starting to make a coffee
+// Grinding coffee beans
+// Boiling water
+// Mixing boiled water with crushed coffee beans
+// Pouring coffee into the cup
+// Pouring some milk into the cup
+// Coffee is ready!`);
+
+console.log("Write how many cups of coffee you will need:");
+let cups = input();
+console.log(`For ${cups} cups of coffee you will need:
+${cups * 200} ml of water
+${cups * 50} ml of milk
+${cups * 15} g of coffee beans`);
